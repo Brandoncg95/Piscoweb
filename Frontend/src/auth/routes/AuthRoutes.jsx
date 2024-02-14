f1ad3eaf647ca392047161dc8cb4b4e92ca29c22
Brandon <bcatanogarcia@gmail.com>
2024-02-14 14:01:07 -0500

    Se realizan las Rutas secundarias, quedan faltando ruta principal con React-Router-dom

---
+++ Frontend/src/auth/routes/AuthRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from '../pages/LoginPage'
+import RegisterPage from '../pages/RegisterPage'
+
+export const AuthRoutes = () => {
+    return (
+        <Routes>
+            <Route path='Login' element={<LoginPage />} />
+            <Route path='Register' element={<RegisterPage />} />
+
+
+            <Route path='/*' element={<Navigate to="/auth/Login" />} />
+        </Routes>
+
+
+    )
+}
+
+export default AuthRoutes
---
+++ Frontend/src/router/Approuter.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+
+
+export const Approuter = () => {
+    return (
+        <Routes>
+            <Route path >
+
+            </Route>
+        </Routes>
+    )
+}
+
+export default Approuter
