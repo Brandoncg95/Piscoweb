a012a8426579f6f8edba8bdbc6fae7f5a6da49f1
Brandon <bcatanogarcia@gmail.com>
2024-02-14 20:51:19 -0500

    Se actualiza las rutas

--- Frontend/src/router/Approuter.jsx
+++ Frontend/src/router/Approuter.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom"
+import AuthRoutes from "../auth/routes/AuthRoutes"
 
 
 export const Approuter = () => {
     return (
         <Routes>
-            <Route path >
+            <Route path="/auth/*" element={ <AuthRoutes/>}>
 
             </Route>
         </Routes>
